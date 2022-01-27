98269a9ddd2e35dcf02aeebb22753f6957eedf32
Luis <luisfbiancardi@gmail.com>
2022-01-27 08:56:13 -0800

    Fix extra semicolon

--- pages/_app.js
+++ pages/_app.js
@@ -35,7 +35,7 @@ export default function MyApp({ Component, pageProps }) {
   return (
     <>
       <GlobalStyle />
-      <Component {...pageProps} />;
+      <Component {...pageProps} />
     </>
   );
 }
