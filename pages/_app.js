6769184852060fdc0696de1101c9ec2af2122a45
Luis <luisfbiancardi@gmail.com>
2022-01-31 09:14:32 -0800

    remove meta tag test

--- pages/_app.js
+++ pages/_app.js
@@ -1,53 +1,41 @@
 function GlobalStyle() {
   return (
-    (
-      <head>
-        <meta
-          name="viewport"
-          content="width=device-width, initial-
-scale=1.0, maximum-scale=1.0, user-
-scalable=no"
-        />
-      </head>
-    ),
-    (
-      <style global jsx>{`
-        * {
-          margin: 0;
-          padding: 0;
-          box-sizing: border-box;
-          list-style: none;
-        }
-        body {
-          font-family: "Open Sans", sans-serif;
-        }
-        /* App fit Height */
-        html,
-        body,
-        #__next {
-          min-height: 100vh;
-          display: flex;
-          flex: 1;
-        }
-        #__next {
-          flex: 1;
-        }
-        #__next > * {
-          flex: 1;
-        }
-        /* ./App fit Height */
+    <style global jsx>{`
+      * {
+        margin: 0;
+        padding: 0;
+        box-sizing: border-box;
+        list-style: none;
+      }
+      body {
+        font-family: "Open Sans", sans-serif;
+      }
+      /* App fit Height */
+      html,
+      body,
+      #__next {
+        min-height: 100vh;
+        display: flex;
+        flex: 1;
+      }
+      #__next {
+        flex: 1;
+      }
+      #__next > * {
+        flex: 1;
+      }
+      /* ./App fit Height */
 
-        ul::-webkit-scrollbar,
-        textarea::-webkit-scrollbar {
-          width: 5px;
-        }
-        ul::-webkit-scrollbar-thumb,
-        textarea::-webkit-scrollbar-thumb {
-          background-color: rgba(36, 39, 52, 0.7);
-          border-radius: 50px;
-        }
-      `}</style>
-    )
+      ul::-webkit-scrollbar,
+      textarea::-webkit-scrollbar {
+        width: 5px;
+      }
+      ul::-webkit-scrollbar-thumb,
+      textarea::-webkit-scrollbar-thumb {
+        background-color: rgba(36, 39, 52, 0.7);
+        border-radius: 50px;
+      }
+    `}</style>
   );
 }
 
